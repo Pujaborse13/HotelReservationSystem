86fb46a293832a9a5c9084600d361e3219f44e1f
Puja Borse <pujaborse13@gmail.com>
2024-12-24 15:15:07 +0530

    Initial commit: Added welcome message in HotelReservation class

---
+++ src/main/java/org/example/HotelReservation.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelReservation
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Hotel Reservation Program" );
+
+    }
+}
