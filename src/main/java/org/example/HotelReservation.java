218bdcc9ef7909458b75e56bbdb0f9574bcb1f61
Puja Borse <pujaborse13@gmail.com>
2024-12-26 15:16:19 +0530

    Ability to find cheapest hotel for given date

--- src/main/java/org/example/HotelReservation.java
+++ src/main/java/org/example/HotelReservation.java
@@ -1,10 +1,11 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
-public class HotelReservation
-{
+public class HotelReservation {
     private List<Hotel> hotels;
 
     public HotelReservation() {
@@ -16,26 +17,44 @@ public class HotelReservation
         Hotel hotel = new Hotel(name,weekDayRate,weekendDayRate,rating);
         hotels.add(hotel);
     }
+    public String findCheapestHotel(String... dates) {
+        Map<Hotel, Integer> hotelCosts = new HashMap<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy");
 
-    public List<Hotel> getHotels() {
-        return hotels;
+        for (Hotel hotel : hotels) {
+            int totalCost = 0;
+            for (String date : dates) {
+                LocalDate parsedDate = LocalDate.parse(date, formatter);
+                DayOfWeek day = parsedDate.getDayOfWeek();
+                if (day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY) {
+                    totalCost += hotel.getWeekendRate();
+                } else {
+                    totalCost += hotel.getWeekdayRate();
+                }
+            }
+            hotelCosts.put(hotel, totalCost);
+        }
+        int minCost = Collections.min(hotelCosts.values());
+        List<String> cheapestHotels = new ArrayList<>();
+        for (Map.Entry<Hotel, Integer> entry : hotelCosts.entrySet()) {
+            if (entry.getValue() == minCost) {
+                cheapestHotels.add(entry.getKey().getName());
+            }
+        }
+
+        return String.join(" and ", cheapestHotels) + " with Total Rates $" + minCost;
     }
-
-    public void displayHotels() {
-        hotels.forEach(System.out::println);
-    }
-
 
     public static void main( String[] args ) {
-        System.out.println( "Welcome to Hotel Reservation Program" );
+        System.out.println("Welcome to Hotel Reservation Program");
 
         HotelReservation reservation = new HotelReservation();
-        reservation.addHotel("Lakewood", 110, 90,3);
-        reservation.addHotel("Bridgewood", 160, 60,4);
-        reservation.addHotel("Ridgewood", 220, 150,5);
+        reservation.addHotel("Lakewood", 110, 90, 3);
+        reservation.addHotel("Bridgewood", 160, 60, 4);
+        reservation.addHotel("Ridgewood", 220, 150, 5);
 
-        System.out.println("Hotels and their rates:");
-        reservation.displayHotels();
+        System.out.println("Cheapest Hotel: " + reservation.findCheapestHotel("11Sep2020", "12Sep2020"));
+
     }
 
 
--- src/test/java/org/example/HotelReservationTest.java
+++ src/test/java/org/example/HotelReservationTest.java
@@ -1,24 +1,19 @@
 package org.example;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HotelReservationTest {
     @Test
-    public void givenHotelDetails_whenAdded_shouldContainWeekdayAndWeekendRates() {
+    public void givenDateRange_shouldReturnCheapestHotel() {
         HotelReservation reservation = new HotelReservation();
         reservation.addHotel("Lakewood", 110, 90, 3);
         reservation.addHotel("Bridgewood", 150, 50, 4);
         reservation.addHotel("Ridgewood", 220, 150, 5);
 
-        List<Hotel> hotels = reservation.getHotels();
-
-        assertEquals(3, hotels.size());
-        assertEquals(110, hotels.get(0).getWeekdayRate());
-        assertEquals(90, hotels.get(0).getWeekendRate());
-        assertEquals(150, hotels.get(1).getWeekdayRate());
-        assertEquals(50, hotels.get(1).getWeekendRate());
-    }
+        String result = reservation.findCheapestHotel("11Sep2020", "12Sep2020");
+        assertEquals("Lakewood and Bridgewood with Total Rates $200", result);
+             }
     }
