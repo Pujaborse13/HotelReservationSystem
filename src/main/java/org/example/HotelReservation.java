6df407ab660cb0f8ad43f47ec6081bdf88eeae25
Puja Borse <pujaborse13@gmail.com>
2024-12-26 18:10:28 +0530

    Ability To Add Ratings To Each Hotel

--- src/main/java/org/example/HotelReservation.java
+++ src/main/java/org/example/HotelReservation.java
@@ -1,8 +1,5 @@
 package org.example;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class HotelReservation {
@@ -10,40 +7,21 @@ public class HotelReservation {
 
     public HotelReservation() {
         hotels = new ArrayList<>();
-
     }
 
     public void addHotel(String name , int weekDayRate , int weekendDayRate, int rating) {
         Hotel hotel = new Hotel(name,weekDayRate,weekendDayRate,rating);
         hotels.add(hotel);
     }
-    public String findCheapestHotel(String... dates) {
-        Map<Hotel, Integer> hotelCosts = new HashMap<>();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy");
 
-        for (Hotel hotel : hotels) {
-            int totalCost = 0;
-            for (String date : dates) {
-                LocalDate parsedDate = LocalDate.parse(date, formatter);
-                DayOfWeek day = parsedDate.getDayOfWeek();
-                if (day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY) {
-                    totalCost += hotel.getWeekendRate();
-                } else {
-                    totalCost += hotel.getWeekdayRate();
-                }
-            }
-            hotelCosts.put(hotel, totalCost);
-        }
-        int minCost = Collections.min(hotelCosts.values());
-        List<String> cheapestHotels = new ArrayList<>();
-        for (Map.Entry<Hotel, Integer> entry : hotelCosts.entrySet()) {
-            if (entry.getValue() == minCost) {
-                cheapestHotels.add(entry.getKey().getName());
-            }
-        }
-
-        return String.join(" and ", cheapestHotels) + " with Total Rates $" + minCost;
+    public List<Hotel> getHotels() {
+        return hotels;
     }
+
+    public void displayHotels() {
+        hotels.forEach(System.out::println);
+    }
+
 
     public static void main( String[] args ) {
         System.out.println("Welcome to Hotel Reservation Program");
@@ -53,8 +31,8 @@ public class HotelReservation {
         reservation.addHotel("Bridgewood", 160, 60, 4);
         reservation.addHotel("Ridgewood", 220, 150, 5);
 
-        System.out.println("Cheapest Hotel: " + reservation.findCheapestHotel("11Sep2020", "12Sep2020"));
-
+        System.out.println("Hotels with their ratings:");
+        reservation.displayHotels();
     }
 
 
--- src/test/java/org/example/HotelReservationTest.java
+++ src/test/java/org/example/HotelReservationTest.java
@@ -7,13 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class HotelReservationTest {
     @Test
-    public void givenDateRange_shouldReturnCheapestHotel() {
+    public void givenHotels_whenAdded_shouldContainRatings() {
         HotelReservation reservation = new HotelReservation();
         reservation.addHotel("Lakewood", 110, 90, 3);
         reservation.addHotel("Bridgewood", 150, 50, 4);
         reservation.addHotel("Ridgewood", 220, 150, 5);
 
-        String result = reservation.findCheapestHotel("11Sep2020", "12Sep2020");
-        assertEquals("Lakewood and Bridgewood with Total Rates $200", result);
-             }
+        assertEquals(3, reservation.getHotels().size());
+        assertEquals(3, reservation.getHotels().get(0).getRating());
+        assertEquals(4, reservation.getHotels().get(1).getRating());
+        assertEquals(5, reservation.getHotels().get(2).getRating());
+        }
     }
